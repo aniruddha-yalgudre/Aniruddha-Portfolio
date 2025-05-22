27e7a18132966225bfb2d3faa009c71314cf2e60
Aniruddha Parshuram Yalgudre <145222690+aniruddha-yalgudre@users.noreply.github.com>
2025-05-22 20:07:44 +0530

    adding verce file

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Aniruddha-Portfolio/', // Set to your subpath if needed (e.g., '/repo-name/')
+  base: process.env.NODE_ENV === 'production' ? '/Aniruddha-Portfolio/' : '/', // Adjust base dynamically based on environment
   plugins: [react()],
   assetsInclude: ['**/*.glsl', '**/*.html'],
   server: {
     host: true,  
     port: 5177,
+  },
+  build: {
+    outDir: 'dist', // Ensure the output directory is correctly set
   },
 })

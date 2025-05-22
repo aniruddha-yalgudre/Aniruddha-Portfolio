ef799cdebd446f7f6b3adbb54ec18e5c730cb225
Aniruddha Parshuram Yalgudre <145222690+aniruddha-yalgudre@users.noreply.github.com>
2025-05-22 20:14:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or vue, svelte, etc.
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Aniruddha-Portfolio/' : '/', // Adjust base dynamically based on environment
   plugins: [react()],
-  assetsInclude: ['**/*.glsl', '**/*.html'],
-  server: {
-    host: true,  
-    port: 5177,
-  },
   build: {
-    outDir: 'dist', // Ensure the output directory is correctly set
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
+  // This is important for Vercel deployments
+  base: './',
 })
